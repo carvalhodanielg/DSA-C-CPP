8fe8cd3e9ee81b13767bbe10b2f3a303d3da5270
carvalhodanielg <carvalhodanielg@gmail.com>
2024-01-27 16:54:42 -0300

    first

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int total = sum(2, 3);
+    printf("Sup, mf, the result is ss %d", total);
+    return 0;
+}
